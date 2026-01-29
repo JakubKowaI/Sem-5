e9738a5ef7f2e5d2c1e58f3543a0dca0e8e27f2a
Jakub Kowal <ikubutek@gmail.com>
2026-01-29 23:03:44 +0100

    Done Z3L7 cwiki jftt

--- Kodowanie/L7/hamming.h
+++ Kodowanie/L7/hamming.h
@@ -13,6 +13,7 @@ constexpr uint8_t wielomianH = 0b00010111; // x^4 + x^2 + x + 1
 
 std::array<uint8_t,4> matrix{};
 std::array<uint8_t,3> matrixH{};
+std::array<uint8_t,7> matrixT{};
 
 void gen_matrix(){
 	for(int y=0;y<4;y++){
@@ -38,12 +39,27 @@ void gen_matrix(){
 			matrix[y]+=1;
 		}
 	}
+
 	// for(auto i : matrix){
 	// 	std::cout<<std::bitset<8>(i)<<std::endl;
 	// }
 
 	for(int i=0;i<3;i++){
 		matrixH[i]=wielomianH<<i;
+	}
+
+	for(int j=6;j>=0;j--){
+		uint8_t tmp=0;
+		for(int i=0;i<4;i++){
+			tmp=tmp<<1;
+			tmp+=(matrix[i]>>j+1)%2;
+		}
+		matrixT[6-j]=tmp;
+	}
+
+	std::cout<<"matrixT: \n";
+	for(auto i : matrixT){
+		std::cout<<std::bitset<8>(i)<<std::endl;
 	}
 }
 
@@ -68,83 +84,99 @@ struct DecodeResult {
 	bool detected_double_error = false;
 };
 
-// inline DecodeResult decode_codeword(uint8_t code8) {
-// 	static std::array<int8_t, 256> code_to_nibble;
-// 	static bool inited = false;
-// 	if (!inited) {
-// 		code_to_nibble.fill(-1);
-// 		for (int m = 0; m < 16; ++m) {
-// 			uint8_t c = encode(static_cast<uint8_t>(m));
-// 			code_to_nibble[c] = static_cast<int8_t>(m);
-// 		}
-// 		inited = true;
-// 	}
-
-// 	DecodeResult r;
-// 	int8_t exact = code_to_nibble[code8];
-// 	if (exact >= 0) {
-// 		r.nibble = static_cast<uint8_t>(exact);
-// 		return r;
-// 	}
-
-// 	// try single-bit correction
-// 	for (int pos = 0; pos < 8; ++pos) {
-// 		uint8_t flipped = static_cast<uint8_t>(code8 ^ (1u << pos));
-// 		int8_t v = code_to_nibble[flipped];
-// 		if (v >= 0) {
-// 			r.nibble = static_cast<uint8_t>(v);
-// 			r.corrected_single_error = true;
-// 			return r;
-// 		}
-// 	}
-
-// 	r.detected_double_error = true;
-// 	return r;
-// }
-
-inline DecodeResult decode_codeword(uint8_t code8){
-	uint8_t syndrom=0;
-	for(int i=0;i<3;i++){
-		uint8_t tmp = matrixH[i] & code8;
-		uint8_t count=0;
-		syndrom = syndrom<<1;
-		for(int j=0;j<7;j++){
-			if((tmp>>j)%2)count++;
+inline DecodeResult decode_codeword(uint8_t code8) {
+	static std::array<int8_t, 256> code_to_nibble;
+	static bool inited = false;
+	if (!inited) {
+		code_to_nibble.fill(-1);
+		for (int m = 0; m < 16; ++m) {
+			uint8_t c = encode(static_cast<uint8_t>(m));
+			code_to_nibble[c] = static_cast<int8_t>(m);
 		}
-		syndrom=syndrom>>1;
-		syndrom=syndrom<<1;
-		if(count%2){
-			syndrom+=1;
-		}
+		inited = true;
 	}
 
 	DecodeResult r;
-
-	//Naprawić przekaz
-	if(syndrom>0){
-		r.corrected_single_error=1;
-		code8 ^= ((uint8_t)1 << syndrom);
-			
-		uint8_t count=0;
-		for(int i=0;i<8;i++){
-			if((code8>>i)%2)count++;
-		}
-		if(count%2)r.detected_double_error=1;
-
-		r.nibble=(code8>>1)^0b1011;
+	int8_t exact = code_to_nibble[code8];
+	if (exact >= 0) {
+		r.nibble = static_cast<uint8_t>(exact);
 		return r;
 	}
-	
 
-	uint8_t count=0;
-	for(int i=0;i<8;i++){
-		if((code8>>i)%2)count++;
+	// try single-bit correction
+	for (int pos = 0; pos < 8; ++pos) {
+		uint8_t flipped = static_cast<uint8_t>(code8 ^ (1u << pos));
+		int8_t v = code_to_nibble[flipped];
+		if (v >= 0) {
+			r.nibble = static_cast<uint8_t>(v);
+			r.corrected_single_error = true;
+			return r;
+		}
 	}
-	if(count%2)r.corrected_single_error=1;
-	//code8 ^= (uint8_t)1;
-	r.nibble=(code8>>1)^0b1011;
+
+	r.detected_double_error = true;
 	return r;
 }
+
+// inline DecodeResult decode_codeword(uint8_t code8){
+// 	uint8_t syndrom=0;
+// 	for(int i=0;i<3;i++){
+// 		uint8_t tmp = matrixH[i] & code8;
+// 		uint8_t count=0;
+// 		syndrom = syndrom<<1;
+// 		for(int j=0;j<7;j++){
+// 			if((tmp>>j)%2)count++;
+// 		}
+// 		syndrom=syndrom>>1;
+// 		syndrom=syndrom<<1;
+// 		if(count%2){
+// 			syndrom+=1;
+// 		}
+// 	}
+
+// 	DecodeResult r;
+
+// 	//Naprawić przekaz
+// 	if(syndrom>0){
+// 		r.corrected_single_error=1;
+// 		code8 ^= ((uint8_t)1 << syndrom);
+			
+// 		uint8_t count=0;
+// 		for(int i=0;i<8;i++){
+// 			if((code8>>i)%2)count++;
+// 		}
+// 		if(count%2)r.detected_double_error=1;
+
+// 		r.nibble=0;
+// 		for(int i=0;i<7;i++){
+// 			//uint8_t tmp =code8>>i;
+// 			if((code8>>i)%2){
+// 				r.nibble^=matrixT[i];
+// 			}
+// 		}
+
+// 		//r.nibble=(code8>>1)^0b1011;
+// 		return r;
+// 	}
+	
+
+// 	uint8_t count=0;
+// 	for(int i=0;i<8;i++){
+// 		if((code8>>i)%2)count++;
+// 	}
+// 	if(count%2)r.corrected_single_error=1;
+// 	//code8 ^= (uint8_t)1;
+// 	r.nibble=0;
+// 	std::cout<<"Dekodowanie\n";
+// 	for(int i=0;i<7;i++){
+		
+// 		if((code8>>(i+1))%2){
+// 			std::cout<<std::bitset<8>(matrixT[i]).to_string()<<std::endl;
+// 			r.nibble^=matrixT[i];
+// 		}
+// 	}
+// 	return r;
+// }
 
 
 }
--- Kodowanie/L7/test.cpp
+++ Kodowanie/L7/test.cpp
@@ -4,4 +4,6 @@ int main(){
     hamming::gen_matrix();
     // std::cout<<std::bitset<8>(hamming::encode(0b00000011))<<std::endl;
     std::cout<<hamming::encode(0b00000011)<<std::endl;
+    //std::cout<<"Coded: \n";
+    std::cout<<std::bitset<8>(hamming::decode_codeword(hamming::encode(0b00000011)).nibble)<<std::endl;
 }
